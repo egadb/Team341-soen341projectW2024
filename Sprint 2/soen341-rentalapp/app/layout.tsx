8139a1501ed57e38457f6196d7ff6566a1182e7c
Sevag Merdkhanian <Sevag.merdkhanian@gmail.com>
2024-03-08 20:30:58 -0500

    changed reservation form, added functionality

--- Sprint 2/soen341-rentalapp/app/layout.tsx
+++ Sprint 2/soen341-rentalapp/app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/global/Footer";
+import NavBar from "@/components/global/NavBar";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -17,9 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        {/* <NavBar /> */}
+        <NavBar />
         {children}
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );
--- Sprint 2/soen341-rentalapp/app/reservation/page.tsx
+++ Sprint 2/soen341-rentalapp/app/reservation/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
-import { useState } from "react";
+import Provider from "@/components/form/Provider";
+import Spinner from "@/components/form/Spinner";
+import { ChangeEvent, useState } from "react";
 
 export default function ReservationForm() {
   const [location, setLocation] = useState("");
@@ -8,91 +10,182 @@ export default function ReservationForm() {
   const [returnDate, setReturnDate] = useState("");
   const [category, setCategory] = useState("");
   const [priceRange, setPriceRange] = useState("");
-  const [additionalFeatures, setAdditionalFeatures] = useState("");
+  const [additionalFeatures, setAdditionalFeatures] = useState<string[]>([]);
 
-  const handleSubmit = (event: { preventDefault: () => void }) => {
-    event.preventDefault();
-    // Handle form submission here
+  const handleChange = (event: ChangeEvent<HTMLSelectElement | HTMLInputElement>) => {
+    const { name, value } = event.target;
+    switch (name) {
+      case "location":
+        setLocation(value);
+        break;
+      case "pickupDate":
+        setPickupDate(value);
+        break;
+      case "returnDate":
+        setReturnDate(value);
+        break;
+      case "category":
+        setCategory(value);
+        break;
+      case "priceRange":
+        setPriceRange(value);
+        break;
+      default:
+        break;
+    }
   };
+  const handleCheckboxChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, checked } = event.target;
+    if (checked) {
+      setAdditionalFeatures([...additionalFeatures, value]);
+    } else {
+      setAdditionalFeatures(additionalFeatures.filter((feature) => feature !== value));
+    }
+  };
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    const formData = new FormData(event.currentTarget);
+    const newLocation = formData.get("location") as string;
+    const newPickupDate = formData.get("pickupDate") as string;
+    const newReturnDate = formData.get("returnDate") as string;
+    const newCategory = formData.get("category") as string;
+    const newPriceRange = formData.get("priceRange") as string;
+
+    setLocation(newLocation);
+    setPickupDate(newPickupDate);
+    setReturnDate(newReturnDate);
+    setCategory(newCategory);
+    setPriceRange(newPriceRange);
+
+    // console.log(location);
+    // console.log(pickupDate);
+    // console.log(returnDate);
+    // console.log(category);
+    // console.log(priceRange);
+    // for (let feature of additionalFeatures) {
+    //   console.log(feature);
+
+    //make it go to the next page that displays cars now
+
+    // }
+  };
+
   return (
-    <div className="h-screen">
-      <div className="h-full rounded-lg bg-red-800 p-5 shadow-lg" style={{ marginTop: "75px" }}>
-        <h1 className="my-4 text-xl font-bold">Reservation</h1>
-        <form onSubmit={handleSubmit} className="h-full">
-          <input
-            type="text"
-            className="rounded-md border-2 p-2 text-black"
-            name="location"
-            placeholder="  Location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
+    <div className="grid h-screen place-items-center">
+      <div className="rounded-lg border-t-4 border-green-400 bg-white p-5 shadow-lg">
+        <h1 className="my-4 text-xl font-bold">Register</h1>
+        <Provider formAction={handleSubmit}>
+          <Spinner />
+          <select
+            id="locationDropdown"
+            className="rounded-md border-2 text-gray-400"
+            name={"location"}
             required
-          />
-          <br />
-          <br />
-          <div className="mb-2">Pickup Date</div>
+            onChange={handleChange}
+          >
+            <option value="" disabled selected>
+              Select a Location
+            </option>
+            <option value="montreal">Montreal</option>
+            <option value="laval">Laval</option>
+            <option value="west-island">West Island</option>
+          </select>
+          Pickup Date
           <input
             type="date"
-            className="rounded-md border-2 p-2 text-black"
+            className="rounded-md border-2 text-gray-400"
             name="pickupDate"
-            placeholder="  Pickup Date"
-            value={pickupDate}
-            onChange={(e) => setPickupDate(e.target.value)}
+            placeholder="Pickup Date"
             required
+            onChange={handleChange}
           />
-          <br />
-          <br />
-          <div className="mb-2">Return Date</div>
+          Return Date
           <input
             type="date"
-            className="rounded-md border-2 p-2 text-black"
+            className="rounded-md border-2 text-gray-400"
             name="returnDate"
-            placeholder="  Return Date"
-            value={returnDate}
-            onChange={(e) => setReturnDate(e.target.value)}
+            placeholder="Return Date"
             required
+            onChange={handleChange}
           />
-          <br />
-          <br />
-          <input
-            type="text"
-            className="rounded-md border-2 p-2 text-black"
-            name="category"
-            placeholder="  Category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
+          <select
+            id="categoryDropdown"
+            className="rounded-md border-2 text-gray-400"
+            name={"category"}
             required
-          />
-          <br />
-          <br />
-          <input
-            type="text"
-            className="rounded-md border-2 p-2 text-black"
-            name="priceRange"
-            placeholder="  Price Range"
-            value={priceRange}
-            onChange={(e) => setPriceRange(e.target.value)}
+            onChange={handleChange}
+          >
+            <option value="" disabled selected>
+              Select a Category
+            </option>
+            <option value="sedan">Sedan</option>
+            <option value="suv">SUV</option>
+            <option value="pickup">Pickup</option>
+            <option value="hatchback">Hatchback</option>
+          </select>
+          <select
+            id="priceDropdown"
+            className="rounded-md border-2 text-gray-400"
+            name={"priceRange"}
             required
-          />
-          <br />
-          <br />
-          <input
-            type="text"
-            className="rounded-md border-2 p-2 text-black"
-            name="additionalFeatures"
-            placeholder="  Additional Features"
-            value={additionalFeatures}
-            onChange={(e) => setAdditionalFeatures(e.target.value)}
-          />
-          <br />
-          <br />
+            onChange={handleChange}
+          >
+            <option value="" disabled selected>
+              Select a Price Range ($/day)
+            </option>
+            <option value="30-50">30$ to 50$</option>
+            <option value="51-80">51$ to 80$</option>
+            <option value="81-120">81$ to 120$</option>
+            <option value="121-and-more">121$ and More</option>
+          </select>
+          <div className="flex flex-wrap">
+            <label className="flex w-full items-center sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <input
+                type="checkbox"
+                name="sunroof"
+                value="sunroof"
+                className="mr-2"
+                onChange={handleCheckboxChange}
+              />
+              <span className="mr-2">Sunroof</span>
+            </label>
+            <label className="flex w-full items-center sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <input
+                type="checkbox"
+                name="leatherSeats"
+                value="leatherSeats"
+                className="mr-2"
+                onChange={handleCheckboxChange}
+              />
+              <span className="mr-2">Leather Seats</span>
+            </label>
+            <label className="flex w-full items-center sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <input
+                type="checkbox"
+                name="heatedSeats"
+                value="heatedSeats"
+                className="mr-2"
+                onChange={handleCheckboxChange}
+              />
+              <span className="mr-2">Heated Seats</span>
+            </label>
+            <label className="flex w-full items-center sm:w-1/2 md:w-1/3 lg:w-1/4">
+              <input
+                type="checkbox"
+                name="backupCamera"
+                value="backupCamera"
+                className="mr-2"
+                onChange={handleCheckboxChange}
+              />
+              <span className="mr-2">Backup Camera</span>
+            </label>
+          </div>
           <button
             type="submit"
             className="cursor-pointer bg-green-600 px-6 py-2 font-bold text-white"
           >
             Submit
           </button>
-        </form>
+        </Provider>
       </div>
     </div>
   );
