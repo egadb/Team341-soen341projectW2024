d8317d56e2d9041e62d0bb3d3d6195cc0f2230ff
davCohen3 <davidcohen15@hotmail.com>
2024-03-10 21:57:26 -0400

    Tailwind for Login and Register

--- Sprint 2/soen341-rentalapp/app/register/page.tsx
+++ Sprint 2/soen341-rentalapp/app/register/page.tsx
@@ -5,47 +5,47 @@ import Link from "next/link";
 
 export default function RegisterForm() {
   return (
-    <div className="grid h-screen place-items-center">
-      <div className="rounded-lg border-t-4 border-green-400 bg-white p-5 shadow-lg">
-        <h1 className="my-4 text-xl font-bold">Register</h1>
+    <div className="grid h-screen place-items-center bg-sky-100">
+      <div className="absolute top-0 right-0 h-full rounded-lg border-t-4 border-sky-900 bg-slate-100 p-5 shadow-lg">
+        <h1 className="my-4 text-4xl px-4 py-20 font-bold">Register</h1>
         <Provider formAction={registerUser}>
           <Spinner />
           <input
             type="text"
-            className="rounded-md border-2"
+            className="w-full h-12 px-4 py-8 rounded-full border-2"
             name={"firstName"}
-            placeholder="First Name"
+            placeholder=" First Name"
             required
           />
           <input
             type="text"
-            className="rounded-md border-2"
+            className="w-full h-12 px-4 py-8 rounded-full border-2"
             name={"lastName"}
-            placeholder="Last Name"
+            placeholder=" Last Name"
             required
           />
           <input
             type="text"
-            className="rounded-md border-2"
+            className="w-full h-12 px-4 py-8 rounded-full border-2"
             name={"email"}
-            placeholder="Email"
+            placeholder=" Email"
             required
           />
           <input
             type="password"
-            className="rounded-md border-2"
+            className="w-full h-12 px-4 py-8 rounded-full border-2"
             name={"password"}
-            placeholder="Password"
+            placeholder=" Password"
             required
           />
           <button
             type="submit"
-            className="cursor-pointer bg-green-600 px-6 py-2 font-bold text-white"
+            className="cursor-pointer bg-sky-900 px-6 py-3 rounded-full font-bold text-white"
           >
             Register
           </button>
         </Provider>
-        <Link className="my-4 text-right text-sm" href={"/login"}>
+        <Link className="px-28 text-right text-sm" href={"/login"}>
           Already have an account? <span className="underline">Login</span>
         </Link>
       </div>
