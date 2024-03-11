8e7c0e03c1e1a8bfdcf850d9675a855e881213df
egadb <egadboisroy@gmail.com>
2024-03-11 10:49:17 -0400

    fix authOptions

--- Sprint 2/soen341-rentalapp/app/api/auth/[...nextauth]/route.tsx
+++ Sprint 2/soen341-rentalapp/app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,7 @@
 import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth";
 
-export const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-export { handler as GET, handler as POST };
+export const GET = handler.handlers.GET;
+export const POST = handler.handlers.POST;
